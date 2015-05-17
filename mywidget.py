ef1fdf338ecea97d6c2f9a0b2ccbd2af7e46cb16
Dustin Spicuzza <dustin@virtualroadside.com>
2015-05-17 19:21:20 -0400

    tabs -> spaces.. must have been present in vala example

--- mywidget.py
+++ mywidget.py
@@ -9,37 +9,37 @@ from gi_composites import GtkTemplate, GtkCallback, GtkChild
 @GtkTemplate(ui='mywidget.ui')
 class MyWidget(Gtk.Box):
 
-	# Required else you would need to specify the full module
-	# name in mywidget.ui (__main__+MyWidget)
-	__gtype_name__ = 'MyWidget'
-	
-	entry = GtkChild()
+    # Required else you would need to specify the full module
+    # name in mywidget.ui (__main__+MyWidget)
+    __gtype_name__ = 'MyWidget'
+    
+    entry = GtkChild()
 
-	def __init__(self, text):
-		super(Gtk.Box, self).__init__()
-		
-		# This must occur *after* you initialize your base
-		self.init_template()
-		
-		self.entry.set_text(text)
-	
-	@GtkCallback
-	def button_clicked(self, widget):
-		print("The button was clicked with entry text: %s" % self.entry.get_text())
+    def __init__(self, text):
+        super(Gtk.Box, self).__init__()
+        
+        # This must occur *after* you initialize your base
+        self.init_template()
+        
+        self.entry.set_text(text)
+    
+    @GtkCallback
+    def button_clicked(self, widget):
+        print("The button was clicked with entry text: %s" % self.entry.get_text())
 
-	@GtkCallback
-	def entry_changed(self, widget):
-		print("The entry text changed: %s" % self.entry.get_text())
+    @GtkCallback
+    def entry_changed(self, widget):
+        print("The entry text changed: %s" % self.entry.get_text())
 
 
 if __name__ == '__main__':
-	
-	win = Gtk.Window()
-	win.connect('delete-event', Gtk.main_quit)
-	
-	widget = MyWidget("The entry text!")
-	win.add(widget)
-	
-	win.show_all()
-	
-	Gtk.main()
+    
+    win = Gtk.Window()
+    win.connect('delete-event', Gtk.main_quit)
+    
+    widget = MyWidget("The entry text!")
+    win.add(widget)
+    
+    win.show_all()
+    
+    Gtk.main()
